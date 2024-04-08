543783b712aaf0d22efa214ea6dc6f1b4c32c5f3
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-04-08 23:49:27 +0500

    Add files via upload

---
+++ your_own_Array.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question no:13;
+let transportation = ['motorcycle', 'car', 'Train', 'bus'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
